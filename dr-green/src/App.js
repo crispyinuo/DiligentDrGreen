ec0b07d4110f9f3c5a10e3b77028f93a3883dd51
許盛雯 <sabrina@Sabrina.local>
2024-04-18 01:37:10 -0700

    History page added

--- dr-green/src/App.js
+++ dr-green/src/App.js
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Description from "./pages/description";
-<<<<<<< HEAD
 import History from "./pages/history";
 
-=======
 import Community from "./pages/community";
->>>>>>> 52fbbb67a0c677034ad254f9604ef6a7db955a07
 
 export default function App() {
   return (
@@ -16,11 +13,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="description" element={<Description />} />
-<<<<<<< HEAD
         <Route path="history" element={<History />} />
-=======
         <Route path="community" element={<Community />} />
->>>>>>> 52fbbb67a0c677034ad254f9604ef6a7db955a07
       </Routes>
     </BrowserRouter>
   );
