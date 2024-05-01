f44778be0d87a54b03c752271b5fabaa680349d4
Zoe Zhou <zoez@usc.edu>
2024-05-01 14:22:01 -0700

    add one more comment

--- dr-green/src/pages/postDetails.js
+++ dr-green/src/pages/postDetails.js
@@ -3,10 +3,30 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import Post from '../components/post';
 import Comments from '../components/comments';
 import TopBar from '../components/topBar';
+import Avatar7 from '../images/Avatar7.png';
+import Avatar8 from '../images/Avatar8.png';
 
 function PostDetails() {
     const location = useLocation();
     const navigate = useNavigate();
+
+    const commentsData = [
+        {
+            name: "Emily Moore",
+            avatar: Avatar7,
+            content: "Congrats on the new addition! For barrel cacti, a mix of 50% potting soil and 50% sand works great. Stick to a pot just bigger than the cactus for best growth.",
+            likeCount: '5',
+            commentCount: '0',
+        },
+        {
+            name: "Chris Jackson",
+            avatar: Avatar8,
+            content: "Great choice! I recommend using a terracotta pot.",
+            likeCount: '3',
+            commentCount: '0',
+        },
+
+    ];
 
     const { name, avatar, img, content, like, comment, tag } = location.state || {
         name: 'Ada Jones',
@@ -29,7 +49,17 @@ function PostDetails() {
                     tag={tag}
                     like={like}
                     comment={comment} />
-                <Comments />
+                {commentsData.map((item, index) => (
+                    <Comments
+                        key={index}
+                        avatar={item.avatar}
+                        name={item.name}
+                        likeCount={item.likeCount}
+                        commentCount={item.commentCount}
+                        time={item.time}
+                        content={item.content}
+                    />
+                ))}
             </div>
         </div>
     );
